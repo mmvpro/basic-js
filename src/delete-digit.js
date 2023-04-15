216cd311a88d9b194140d3589c8e20889d9212d2
mmvpro <dublibum@gmail.com>
2023-04-15 17:10:05 +0300

    done: encode-line

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let max = 0;
+  const nStr = n+"";
+  for (let i = 0; i < nStr.length; i++) {
+    const sum = parseInt(nStr.substring(0, i) + nStr.substring(i + 1));
+    if (sum > max) {
+      max = sum;
+    }
+  }
+  return max;
 }
 
 module.exports = {
--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let res = "";
+  let n = 1;
+  let char = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      n++;
+      char = str[i];
+      continue;
+    }
+  
+    res += n !== 1 ? n : "";
+    n = 1;
+    res += char;
+    char = "";
+    if (str[i + 1] !== str[i]) res += str[i];
+  }
+ 
+  
+    res += n !== 1 ? n : "";
+    res += char
+
+  return res;
 }
 
 module.exports = {
