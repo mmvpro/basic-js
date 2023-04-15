9313bb33b8479fd18a0325ad19c527b0b877ce30
mmvpro <dublibum@gmail.com>
2023-04-15 17:25:38 +0300

    done: get-email-domain

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
+function renameFiles(names) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+ const res = [];
+  const counts = {};
+
+  for (const name of names) {
+    if (counts[name]) {
+      const newName = name + `(${counts[name]})`;
+      counts[name]++;
+      counts[newName] = 1; 
+      res.push(newName);
+    } else {
+      counts[name] = 1; 
+      res.push(name);
+    }
+  }
+
+  return res;
 }
 
 module.exports = {
