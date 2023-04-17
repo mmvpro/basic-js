da5e0be33f8a73b3559c452fda125ac0236d5472
mmvpro <dublibum@gmail.com>
2023-04-17 21:40:57 +0300

    done: mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+  const res = [];
+
+  for (let i = 0; i < numRows; i++) {
+    const row = [];
+    for (let j = 0; j < numCols; j++) {
+      let n = 0;
+      for (let ii = i - 1; ii <= i + 1; ii++) {
+        for (let jj = j - 1; jj <= j + 1; jj++) {
+          if (ii >= 0 && ii < numRows && jj >= 0 && jj < numCols && !(ii === i && jj === j) && matrix[ii][jj]) {
+            n++;
+          }
+        }
+      }
+      row.push(n);
+    }
+    res.push(row);
+  }
+
+  return res;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,20 +20,72 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.square = this.generateSquare();
+  }
 
-  
-  
+  generateSquare() {
+    const a = this.alphabet;
+    let sq = [];
+    for (let i = 0; i < a.length; i++) {
+      sq[i] = a.slice(i).concat(a.slice(0, i));
+    }
+
+    return sq;
+  }
+
   encrypt(message, key) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.crypt(message, key, true);
   }
-  decrypt(message, key) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-        
 
+  decrypt(message, key) {
+    return this.crypt(message, key, false);
   }
+
+  crypt(message, key, encrypt) {
+    if (!message || !key) {
+      throw Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    while (key.length < message.length) {
+      key += key;
+    }
+
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] !== ' ' && this.alphabet.includes(message[i])) {
+        if (encrypt) {
+          result +=
+            this.square[this.alphabet.indexOf(message[i])][
+            this.alphabet.indexOf(key[keyIndex])
+            ];
+        } else {
+          result +=
+            this.alphabet[
+            this.square[this.alphabet.indexOf(key[keyIndex])].indexOf(
+              message[i]
+            )
+            ];
+        }
+        keyIndex++;
+      } else {
+        result += message[i];
+      }
+    }
+
+    if (!this.isDirect) {
+      return result.split('').reverse().join('');
+    }
+
+    return result;
+  } 
 }
 
 module.exports = {
