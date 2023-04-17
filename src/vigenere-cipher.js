d42b9df3b2fea5bef5b81a8d491cd87adf9f01dd
mmvpro <dublibum@gmail.com>
2023-04-17 18:17:41 +0300

    done: sum-digits

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -21,55 +21,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class VigenereCipheringMachine {
   
-  constructor(alphabet) {
-    this.alphabet = alphabet;
-    this.square = [];
-}
 
-generateSq() {
-    for (let i = 0; i < this.alphabet.length; i++) {
-        let row = this.alphabet.slice(i);
-        row += this.alphabet.slice(0, i);
-        this.square.push(row);
-    }
-}
-getSq() {
-    return this.square;
-}
   
   
   encrypt(message, key) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-     if (!message || !key) {
-      throw new Error('Incorrect arguments!');
-    }
-    let encryptMessage = "";
-    let newKey = repeatString(key, message);
-    this.generateSq();
-    for (let it = 0; it < message.length; it++) {
-        let i = this.alphabet.indexOf(message[it]);
-        let j = this.alphabet.indexOf(newKey[it]);
-        encryptMessage += this.square[i][j];
-    }
-    return encryptMessage.toLocaleUpperCase();
   }
   decrypt(message, key) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-     if (!message || !key) {
-      throw new Error('Incorrect arguments!');
-    }
-    let decryptMessage = "";
-    let newKey = repeatString(key, message);
-    this.generateSq();
-    for (let it = 0; it < message.length; it++) {
         
-        let i = this.alphabet.indexOf(newKey[it]);
-        let j = this.square[i].indexOf(message[it]);
-        decryptMessage += this.alphabet[j];
-    }
-    return decryptMessage.toLocaleUpperCase();
 
   }
 }
